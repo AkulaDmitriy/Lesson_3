cd865df5fcbd00f8f773b8d6c39c7421d5365a18
AkulaDmitriy <dmitry.akula1985@gmail.com>
2023-02-20 08:54:43 +0300

    add studentRegistrationFromTest

---
+++ src/test/java/DemoqaTest.java
@@ -0,0 +1,56 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DemoqaTest {
+
+    @BeforeAll
+    static void beforeAll() {
+
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+
+    }
+
+    @Test
+    void studentRegistrationFormTest ()
+    {
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        $("#firstName").setValue("Dima");
+        $("#lastName").setValue("Akula");
+
+        $("#userEmail").setValue("Dima_Akula@gmail.com");
+
+        $("#gender-radio-1").sendKeys(" ");
+
+        $("#userNumber").setValue("9999999999");
+
+        $("#dateOfBirthInput").setValue("03 Aug 1985").pressEnter();
+
+        $("#subjectsInput").setValue("Math").pressEnter();// где взять этот локатор
+
+        $("#hobbies-checkbox-1").sendKeys(" ");
+
+        $("#uploadPicture").uploadFile(new File("src/test/data/smile.jpg"));
+
+        $("#currentAddress").setValue("Current Address");
+
+        $("#state").click();
+        $("#react-select-3-option-0").click();
+        $("#city").click();
+        $("#react-select-4-option-0").click();
+
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+
+    }
+}

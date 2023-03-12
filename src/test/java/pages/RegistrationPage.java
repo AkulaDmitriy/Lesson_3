199df25a1d37bfd63a832bc8537afde8e8b37e59
AkulaDmitriy <dmitry.akula1985@gmail.com>
2023-03-12 13:18:45 +0300

    Lesson 7

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,152 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.RegistrationResultModal;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationPage {
+
+    RegistrationResultModal registrationResultModal = new RegistrationResultModal();
+    private SelenideElement lastNameInput = $("#lastName"),
+            firstNameInput = $("#firstName"),
+            emailInput = $("#userEmail"),
+            genderValue = $("#gender-radio-1"),
+            phoneNumberInput = $("#userNumber"),
+            dateOfBirthInput = $("#dateOfBirthInput"),
+            subjectInput = $("#subjectsInput"),
+            hobbiesInput = $("#hobbies-checkbox-1"),
+            fileUpload = $("#uploadPicture"),
+            addressInput = $("#currentAddress"),
+            state = $("#state"),
+            choseState = $("#react-select-3-option-0"),
+            cityinput = $("#city"),
+            choseCity = $("#react-select-4-option-0"),
+            submitForm = $("#submit"),
+            submitMsg = $("#example-modal-sizes-title-lg"),
+            resultForm = $(".modal-body");
+
+    public RegistrationPage openPage() {
+
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String value) {
+
+        firstNameInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+
+        lastNameInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setEmail(String value) {
+
+        emailInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setGender() {
+
+        genderValue.sendKeys(" ");
+        return this;
+    }
+
+    public RegistrationPage setPhoneNumber(String value) {
+
+        phoneNumberInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirth(String value) {
+
+        dateOfBirthInput.setValue("03 Aug 1985").pressEnter();
+        return this;
+
+    }
+
+    public RegistrationPage setSubject(String value) {
+
+        subjectInput.setValue(value).pressEnter();
+        return this;
+
+    }
+
+    public RegistrationPage setHobbies() {
+
+        hobbiesInput.sendKeys(" ");
+        return this;
+
+    }
+
+    public RegistrationPage uploadFile(String value) {
+
+        fileUpload.uploadFile(new File(value));
+        return this;
+
+    }
+
+    public RegistrationPage setAddress(String value) {
+
+        addressInput.setValue(value);
+        return this;
+
+    }
+
+    public RegistrationPage setstate() {
+
+        state.click();
+        return this;
+    }
+
+    public RegistrationPage setChoseState() {
+
+        choseState.click();
+        return this;
+    }
+
+    public RegistrationPage setCity() {
+
+        cityinput.click();
+        return this;
+    }
+
+    public RegistrationPage choseCity() {
+
+        choseCity.click();
+        return this;
+    }
+
+    public RegistrationPage submitForm() {
+
+        submitForm.click();
+        return this;
+    }
+
+    public RegistrationPage checkSubmitMsg(String value) {
+
+        submitMsg.shouldHave(text(value));
+        return this;
+    }
+
+    public RegistrationPage verifyResultModalAppears() {
+
+        registrationResultModal.verifyModalAppears();
+        return this;
+    }
+    public RegistrationPage verifyResult (String key, String value) {
+
+        registrationResultModal.verifyResult(key, value);
+
+        return this;
+    }
+}
---
+++ src/test/java/tests/DemoqaPageObjectsTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoqaPageObjectsTest extends TestBase {
+
+
+    @Test
+    void studentRegistrationFormTest() {
+        String firstName = "Dima",
+                lastName = "Akula",
+                email = "Dima_Akula@gmail.com",
+                phoneNumber = "999999999999",
+                dateOfBirth = "03 Aug 1985",
+                subject = "Math",
+                filePath = "src/test/resources/Img.png",
+                address = "Current Address",
+                submitMessage = "Thanks for submitting the form";
+
+        registrationPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender()
+                .setPhoneNumber(phoneNumber)
+                .setDateOfBirth(dateOfBirth)
+                .setSubject(subject)
+                .setHobbies()
+                .uploadFile(filePath)
+                .setAddress(address)
+                .setstate()
+                .setChoseState()
+                .setCity()
+                .choseCity()
+                .submitForm()
+                .checkSubmitMsg(submitMessage)
+                .verifyResultModalAppears()
+                .verifyResult ("Student Name", firstName+ " " + lastName)
+                .verifyResult("Student Email", email);
+
+    }
+}
